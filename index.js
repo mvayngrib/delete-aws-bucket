ab23365b4c0f31ee9621ed7072afad0e41896f43
Mark Vayngrib <mvayngrib@gmail.com>
2018-10-29 21:30:47 -0400

    refactor: arguments

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-const emptyBucket = require('empty-aws-bucket');
-const aws = require('aws-sdk');
-const s3 = new aws.S3();
+const emptyBucket = require('empty-aws-bucket')
 
-const deleteBucket = async (Bucket, options) => {
-
-  if (options) {
-    aws.config.update(options);
-  }
+const deleteBucket = async ({ s3, bucket }) => {
+  const Bucket = bucket
 
   // if AWS SDK debugging is enabled, log our actions
   const debug = (...args) => {
     if (process.env.AWSJS_DEBUG) {
-      console.log(`delete-aws-bucket | ${Bucket} |`, ...args);
+      console.log(`delete-aws-bucket | ${Bucket} |`, ...args)
     }
   }
 
   // before we can delete the bucket, we must delete all versions of all objects
-  await emptyBucket(Bucket, options);
+  await emptyBucket({ s3, bucket })
 
   // finally, delete the empty bucket
   try {
-    debug('Deleting bucket');
-    await s3.deleteBucket({ Bucket }).promise();
+    debug('Deleting bucket')
+    await s3.deleteBucket({ Bucket }).promise()
   } catch (e) {
     if (e.code === 'NoSuchBucket') {
-      debug('Bucket not found!');
+      debug('Bucket not found!')
     } else {
-      throw e;
+      throw e
     }
   }
-};
+}
 
-module.exports = deleteBucket;
+module.exports = deleteBucket
